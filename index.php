7d11960f0830b8a02e5171a29a170817973f9a99
maxcomodo7 <41265159+maxcomodo7@users.noreply.github.com>
2018-07-15 21:06:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require "vendor/autoload.php";
 $access_token = 'h6mtWI5v4AnNByl38IuofzhQ22O1Hgz4BefbMtHpp8iX9GSmBdE2tNMw5ZrIXdSrm8ySuUV0aZAfCdiVOPOjVWh63VF5TeblHiKlPj5GbPnyKvla1W4d+y0dgUbUTV4UkxVohrWhqC2eQI6dQsheSQdB04t89/1O/w1cDnyilFU=
 ';
 $channelSecret = '63137591c3e2ae3ed0b7fe6da3369978';
-$idPush = 'Ucdcd57e0f331c2053a61ef8e114dce53';
+$idPush = 'Ucdcd57e0f331c2053a61ef8e114dce53'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
