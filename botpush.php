49baca61da5490a73ec8e02a6d1622a98e94537e
maxcomodo7 <41265159+maxcomodo7@users.noreply.github.com>
2018-11-08 15:32:13 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,9 +1,9 @@
 
 <?php
 require "vendor/autoload.php";
-$access_token = '99b832e0d5bcfcfbce3739874efef43c';
-$channelSecret = 'QI9yH61LtbLUjJkZ4oQr/ukn9ODZDVGGO1c5T1BB8wWnlWhrT39tXSoWr8iXjtbvd/mqCQQjHZzID6S7P8oKeeSu93TVI2AcmBlC7iokXRYo6XomDo7DfEdk57XfX+V9ZQrFv5wSX3kOCqUVf6nx2gdB04t89/1O/w1cDnyilFU=';
-$idPush = 'U80ad520d26c5d3bde39e62139780767b'
+$access_token = 'b522c397e38192ce9542775a23dd9ba7';
+$channelSecret = 'xbrrzERPKP5rcZE7eDdNnRAzvF/lJ9yk8xNsMTtVbKk2gr35pKBIhmMm9Gn4xG6uYtxryHEzNdaBzxGRGw67vvZHF3yoPpcMV4k9A/MYnrcd7DRvKwccqkBg9UbmGpcV5mIGDU2GHZOLKctuHey8MgdB04t89/1O/w1cDnyilFU=';
+$idPush = 'U894bbc0369203954038647c5232ebccc'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
